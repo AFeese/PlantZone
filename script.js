0903f4de7c5d4c07040cd3d3184a4e751af6a27f
AFeese <58886725+AFeese@users.noreply.github.com>
2022-03-16 10:46:19 -0400

    suggested plant is now displayed for north carolina, image only. Need to work on updating plant name after state selection.

--- script.js
+++ script.js
@@ -5,21 +5,27 @@
 // }
 
 function stateDropDown() {
-  var stateList = document.getElementById("stateList");
+	var stateList = document.getElementById("stateList");
 
-  document.getElementById("selectedState").value =
-    stateList.options[stateList.selectedIndex].text;
+	document.getElementById("selectedState").value =
+		stateList.options[stateList.selectedIndex].text;
 
-  // if ("selectedState".value === "North Carolina") {
-  //   document.getElementById("zoneNumber").value = "7";
-  // }
+	//need to create loop? or array? for selected state.
+	//Might save some space.
 
-  // if ("selectedState".value === "North Carolina") {
-  //   document.getElementById("zoneNumber").value = "7";
-  // }
+	if (document.getElementById("selectedState").value === "Arizona") {
+		document.getElementById("zoneNumber").value = "2 through 5";
+	}
 
-  if (document.getElementById("selectedState").value === "North Carolina") {
-    document.getElementById("zoneNumber").value = "7";
-  }
-  console.log(document.getElementById("selectedState").value);
+	if (document.getElementById("selectedState").value === "Colorado") {
+		document.getElementById("zoneNumber").value = "4 and 5";
+	}
+
+	if (document.getElementById("selectedState").value === "North Carolina") {
+		document.getElementById("zoneNumber").value = "7";
+		document.getElementById("plantPhoto").text = "Japanese Maple";
+		document.getElementById("displayPhoto").src = "images/japanese_maple.jpg";
+	}
+	console.log(document.getElementById("selectedState").value);
+	console.log(document.getElementById("plantPhoto").text);
 }
