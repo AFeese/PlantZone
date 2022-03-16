182037acd9772700896e6476a9122b17b0540f33
AFeese <58886725+AFeese@users.noreply.github.com>
2022-03-15 21:27:43 -0400

    wrote corresponding zone display for North Carolina state selection.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// function stateDropDown() {
+//   var stateList = document.getElementById("stateList");
+//   document.getElementById("selectedState").value =
+//     stateList.options[stateList.selectedIndex].text;
+// }
+
 function stateDropDown() {
   var stateList = document.getElementById("stateList");
+
   document.getElementById("selectedState").value =
     stateList.options[stateList.selectedIndex].text;
+
+  // if ("selectedState".value === "North Carolina") {
+  //   document.getElementById("zoneNumber").value = "7";
+  // }
+
+  // if ("selectedState".value === "North Carolina") {
+  //   document.getElementById("zoneNumber").value = "7";
+  // }
+
+  if (document.getElementById("selectedState").value === "North Carolina") {
+    document.getElementById("zoneNumber").value = "7";
+  }
+  console.log(document.getElementById("selectedState").value);
 }
